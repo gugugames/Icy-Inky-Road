e56c4f4ff75ffa18087aa7071dcc31082fd77fa1
csh <tizkzks96@gmail.com>
2019-05-04 02:03:54 +0900

    MapEditor database, Button, sample Button 추가 - 진행중

---
+++ Assets/CSH/Script/MapEditor/Editor/WallEditors.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+
+[CustomEditor(typeof(WallSelector))]
+public class WallEditors : Editor {
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        //base.OnInspectorGUI();
+
+        WallSelector wall = target as WallSelector;
+
+        GUILayout.Label("Wall Select");
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Wall1")) {
+            wall.SetWall(0);
+        }
+        if (GUILayout.Button("Wall2")) {
+            wall.SetWall(1);
+        }
+        if (GUILayout.Button("Wall3")) {
+            wall.SetWall(1);
+        }
+
+        GUILayout.EndHorizontal();
+
+        //if (wall.selectedWall != SelectWall.select)
+        //{
+        //    wall.SetWall();
+        //    wall.selectedWall = SelectWall.select;
+        //}
+
+    }
+
+}
---
+++ GlobalSuppressions2.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("코드 품질", "IDE0051:사용되지 않는 private 멤버 제거", Justification = "<보류 중>", Scope = "member", Target = "~M:WallEditors.Start")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("코드 품질", "IDE0051:사용되지 않는 private 멤버 제거", Justification = "<보류 중>", Scope = "member", Target = "~M:WallEditors.Update")]
+
