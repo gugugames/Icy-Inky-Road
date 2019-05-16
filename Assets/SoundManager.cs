a68f708606eca120035255c4cb6978824027b52d
csh <tizkzks96@gmail.com>
2019-05-17 02:29:05 +0900

    bmg, soundmanager 추가

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
