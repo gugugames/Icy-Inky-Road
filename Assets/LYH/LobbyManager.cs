576b57a62e91779400c321d306e56bfde423c465
developerYHL <developer.yhl@gmail.com>
2019-05-20 14:44:19 +0900

    Photon 재설치 / 이용환, 최승호 Merge

---
+++ Assets/LYH/LobbyManager.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+    public byte playerNumber = 2;
+
+    // 로드할 씬 인덱스
+    private int sceneIndex = 1;
+
+    public Text connectionInfoText;
+    public Button joinButton;
+
+    // 룸 옵션
+    RoomOptions ros = new RoomOptions();
+
+    void Awake()
+    {
+        Screen.SetResolution(1920, 1080, false);
+    }
+
+    void Start()
+    {
+        // 접속에 필요한 정보(게임 버전) 설정
+        PhotonNetwork.GameVersion = gameVersion;
+        // 설정한 정보로 마스터 서버 접속 시도
+        PhotonNetwork.ConnectUsingSettings();
+
+        // 룸 접속 버튼 잠시 비활성화
+        joinButton.interactable = false;
+        // 접속 시도 중임을 텍스트로 표시
+        connectionInfoText.text = "마스터 서버에 접속 중...";
+    }
+
+    // 마스터 서버 접속 성공 시 자동 실행
+    public override void OnConnectedToMaster()
+    {
+        joinButton.interactable = true;
+        connectionInfoText.text = "온라인 : 마스터 서버와 연결됨";
+    }
+
+    // 마스터 서버 접속 실패 시 자동 실행
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        joinButton.interactable = false;
+        connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음\n 접속 재시도 중...";
+        // PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // 룸 접속 시도
+    public void Connect()
+    {
+        joinButton.interactable = false;
+
+        if (PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "룸에 접속...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음\n접속 재시도 중...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    // (빈 방이 없어)랜덤 룸 참가에 실패한 경우 자동 실행
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        connectionInfoText.text = "빈 방이 없음, 새로운 방 생성...";
+
+        ros.MaxPlayers = playerNumber;
+        PhotonNetwork.CreateRoom(null, ros);
+    }
+
+    public void Play()
+    {
+        connectionInfoText.text = "참여 인원 " + PhotonNetwork.PlayerList.Length + "/2 ...";
+
+        if (PhotonNetwork.PlayerList.Length >= 2)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            PhotonNetwork.LoadLevel("MainServer");
+        }
+    }
+
+    // 룸에 참가 완료된 경우 자동 실행
+    public override void OnJoinedRoom()
+    {
+        Play();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Play();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        connectionInfoText.text = "참여 인원 " + PhotonNetwork.PlayerList.Length + "/2 ...";
+    }
+}
---
+++ Assets/LYH/ServerManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ServerManager : MonoBehaviourPunCallbacks, IPunObservable
+{
+
+    // 외부에서 싱글톤 오브젝트를 가져올때 사용할 프로퍼티
+    public static ServerManager instance
+    {
+        get
+        {
+            // 만약 싱글톤 변수에 아직 오브젝트가 할당되지 않았다면
+            if (m_instance == null)
+            {
+                // 씬에서 GameManager 오브젝트를 찾아 할당
+                m_instance = FindObjectOfType<ServerManager>();
+            }
+
+            // 싱글톤 오브젝트를 반환
+            return m_instance;
+        }
+    }
+
+    private static ServerManager m_instance; // 싱글톤이 할당될 static 변수
+    public GameObject playerPrefab; // 생성할 플레이어 캐릭터 프리팹
+
+    private void Awake()
+    {
+        // 씬에 싱글톤 오브젝트가 된 다른 GameManager 오브젝트가 있다면
+        if (instance != this)
+        {
+            // 자신을 파괴
+            Destroy(gameObject);
+        }
+    }
+
+    // 게임 시작과 동시에 플레이어가 될 게임 오브젝트를 생성
+    private void Start()
+    {
+        // 생성할 랜덤 위치 지정
+        Vector3 randomSpawnPos = Random.insideUnitSphere * 5f;
+        PhotonNetwork.Instantiate(playerPrefab.name, randomSpawnPos, Quaternion.identity);
+        // 네트워크 상의 모든 클라이언트들에서 생성 실행
+        // 단, 해당 게임 오브젝트의 주도권은, 생성 메서드를 직접 실행한 클라이언트에게 있음
+
+        /*if (photonView.IsMine)
+        {
+            Debug.Log("IsMine");
+            Vector3 SpawnPos1 = new Vector3(4.5f, 0.5f, 4.5f);
+
+            PhotonNetwork.Instantiate(playerPrefab.name, SpawnPos1, Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("No");
+            Vector3 SpawnPos2 = new Vector3(-4.5f, 0.5f, -4.5f);
+
+            PhotonNetwork.Instantiate(playerPrefab.name, SpawnPos2, Quaternion.identity);
+        }*/
+    }
+
+    // 주기적으로 자동 실행되는, 동기화 메서드
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // 로컬 오브젝트라면 쓰기 부분이 실행됨
+        if (stream.IsWriting)
+        {
+            // 네트워크를 통해 score 값을 보내기
+        }
+        else
+        {
+            // 리모트 오브젝트라면 읽기 부분이 실행됨
+            // 네트워크를 통해 score 값 받기
+        }
+    }
+}
