06035b885ad8530c5210267c798c8bf37cc2745b
developerYHL <developer.yhl@gmail.com>
2019-07-08 02:23:09 +0900

    Add an Item interface

---
+++ Assets/LYH/PlayerState.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class PlayerState : PlayerData
+{
+    // 그리드충돌 이벤트로 교체
+    private void OnTriggerEnter(Collider other)
+    {
+        // 아이템과 충돌한 경우 해당 아이템을 사용하는 처리
+        // 사망하지 않은 경우에만 아이템 사용가능
+        if (!dead)
+        {
+            // 충돌한 상대방으로 부터 Item 컴포넌트를 가져오기 시도
+            IItem item = other.GetComponent<IItem>();
+
+            // 충돌한 상대방으로부터 Item 컴포넌트가 가져오는데 성공했다면
+            if (item != null)
+            {
+                // 호스트만 아이템 직접 사용 가능
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    // Use 메서드를 실행하여 아이템 사용
+                    item.Use(gameObject);
+                }
+            }
+        }
+    }
+}
